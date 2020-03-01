3d7fee44ae47c380e6abbf4544c3788039a37ab3
Jathurshan Theivikaran <theivikaran.jathurshan@gmail.com>
2020-02-29 22:50:00 -0500

    Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FillShape
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFileColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(814, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileOpen,
+            this.toolStripMenuItem1,
+            this.mnuFileColor,
+            this.toolStripMenuItem2,
+            this.mnuFileExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.Size = new System.Drawing.Size(152, 22);
+            this.mnuFileOpen.Text = "Open";
+            this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // mnuFileColor
+            // 
+            this.mnuFileColor.Name = "mnuFileColor";
+            this.mnuFileColor.Size = new System.Drawing.Size(152, 22);
+            this.mnuFileColor.Text = "Fill Color...";
+            this.mnuFileColor.Click += new System.EventHandler(this.mnuFileColor_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(152, 22);
+            this.mnuFileExit.Text = "Exit";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 570);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Grid Drawing";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileColor;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using Ambro.Drawing;
+
+namespace FillShape
+{
+    /* Jathurshan Theivikaran
+     * December 13, 2019
+     * This program colours in empty spaces of a picture
+     */
+
+    //sample text files are in the folder of the solution
+    public partial class frmMain : Form
+    {
+        //form variables
+        private Grid grid;
+        private char[,] shape;
+        private Color fillColor;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            //initialize grid
+            grid = new Grid(15, 40, 0, 30, 20);
+
+            //default fill color to blue
+            fillColor = Color.Maroon;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            //draw grid          
+            grid.DrawGrid(e.Graphics);
+        }
+
+        private void frmMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            //determine which box the user clicked
+            int col = e.X / 20;
+            int row = (e.Y - 30) / 20;
+            
+            //fill shape if within shape or fill outside
+            FillShape(row, col);
+
+            //set up the grid cell components to display colour accordingly
+            ConfigureGrid();
+
+            //refesh to call OnPaint 
+            this.Refresh();
+        }
+
+        //call FillShape method
+        public void FillShape(int r, int c)
+        {
+            //check if index is in bounds of array
+            if (r >= 0 && r < shape.GetLength(0) && c >= 0 && c < shape.GetLength(1))
+            {
+                //check if the box colour is white
+                if (shape[r, c] == '.')
+                {
+                    //colour in white box with fillcolour
+                    shape[r, c] = 'F';
+
+                    //pass box that is next to current box in all four direction
+                    FillShape(r + 1, c);
+                    FillShape(r, c + 1);
+                    FillShape(r - 1, c);
+                    FillShape(r, c - 1);
+                }
+            }
+        }
+
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            //open selected file
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            //check if file is a readable text file
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(fileDialog.OpenFile());
+
+                //transfer text file values into array
+                string line = sr.ReadLine();
+                int cols = int.Parse(line);
+                line = sr.ReadLine();
+                int rows = int.Parse(line);
+                shape = new char[rows, cols];
+
+                for (int r = 0; r < rows; r++)
+                {
+                    line = sr.ReadLine();
+                    for (int c = 0; c < cols; c++)
+                    {
+                        shape[r, c] = line[c];
+                    }
+                }
+                //draw grid
+                ConfigureGrid();
+            }
+            else
+            {
+            }
+
+            //refresh form
+            this.Refresh();
+        }
+
+        private void ConfigureGrid()
+        {
+            //store number of rows and columns of grid
+            int rows = shape.GetLength(0);
+            int cols = shape.GetLength(1);
+
+            //loop through each of the elements in grid
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    //change colour of each box depending on character
+                    if (shape[r, c] == '.') grid.getCell(r, c).setBackColor(Color.White);
+                    else if (shape[r, c] == 'T') grid.getCell(r, c).setBackColor(Color.Black);
+                    else if (shape[r, c] == 'F') grid.getCell(r, c).setBackColor(fillColor);
+                }
+            }
+        }
+
+        private void mnuFileColor_Click(object sender, EventArgs e)
+        {
+            //create variable to store selected colour
+            ColorDialog cd = new ColorDialog();
+
+            //check if colour is selected
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                //change fillcolour to selected colour
+                fillColor = cd.Color;
+            }
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            //close form
+            this.Close();
+        }
+    }
+}
